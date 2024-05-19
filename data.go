3d85fafaf1954b73abc83ec615810cea76db90a3
Caio "Quintas_" Santiago <13594102+Quinntas@users.noreply.github.com>
2024-05-19 16:19:53 -0300

    Initial commit

---
+++ data.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/charmbracelet/bubbles/list"
+
+func (b *Board) initLists() {
+	b.cols = []column{
+		newColumn(todo),
+		newColumn(inProgress),
+		newColumn(done),
+	}
+	b.cols[todo].list.Title = "To Do"
+	b.cols[todo].list.SetItems([]list.Item{
+		NewTask(todo, "write kanban board", "it's a demo, so don't worry"),
+		NewTask(todo, "write documentation", "it's a demo, so don't worry"),
+		NewTask(todo, "write tests", "it's a demo, so don't worry"),
+	})
+	b.cols[inProgress].list.Title = "In Progress"
+	b.cols[inProgress].list.SetItems([]list.Item{
+		NewTask(inProgress, "write kanban board", "it's a demo, so don't worry"),
+		NewTask(inProgress, "write documentation", "it's a demo, so don't worry"),
+	})
+	b.cols[done].list.Title = "Done"
+	b.cols[done].list.SetItems([]list.Item{
+		NewTask(done, "write kanban board", "it's a demo, so don't worry"),
+	})
+}
